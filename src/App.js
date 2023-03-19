286196ec247cb9ca759b5dfee94acfe1690fc8c6
Sammy <sammyolsson@gmail.com>
2023-03-19 20:19:38 +0100

    Started the project, added TaskList-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import TaskList from './Components/TaskList'
 
 export const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [loading, setLoading] = useSate(false);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = () => {
+    setLoading(true);
+    fetch('https://week7-backend.herokuapp.com/tasks')
+      .then(res => res.json())
+      .then(data => setTaskList(data))
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <TaskList />
     </div>
   )
 }
