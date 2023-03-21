6a2602418ebe0baa678463f85c21d4bc2525ac4e
Sammy <sammyolsson@gmail.com>
2023-03-21 20:21:08 +0100

    Done with example project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import TaskList from './Components/TaskList'
+import TaskForm from './Components/TaskForm'
 
 export const App = () => {
   const [taskList, setTaskList] = useState([]);
-  const [loading, setLoading] = useSate(false);
+  const [loading, setLoading] = useState(false);
   const [newTodo, setNewTodo] = useState('');
+
+  const fetchTasks = () => {
+    setLoading(true);
+    fetch('https://week-7-backend.onrender.com/tasks')
+      .then((res) => res.json())
+      .then((data) => setTaskList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
 
   useEffect(() => {
     fetchTasks();
   }, []);
 
-  const fetchTasks = () => {
-    setLoading(true);
-    fetch('https://week7-backend.herokuapp.com/tasks')
-      .then(res => res.json())
-      .then(data => setTaskList(data))
-      .catch(error => console.error(error))
-      .finally(() => setLoading(false));
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        description: newTodo
+      })
+    }
+
+    fetch('https://week-7-backend.onrender.com/tasks', options)
+      .then((res) => res.json())
+      .then(() => fetchTasks())
+      .finally(() => setNewTodo(''));
   }
 
   return (
     <div>
-      <TaskList />
+      <TaskForm
+        newTodo={newTodo}
+        onNewTodoChange={handleNewTodoChange}
+        onFormSubmit={onFormSubmit} />
+      <TaskList
+        loading={loading}
+        taskList={taskList}
+        setTaskList={setTaskList} />
     </div>
   )
 }
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,7 +1,32 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
+import { formatRelative } from 'date-fns';
 
-const TaskList = () => {
-  return <div>Task list</div>
+const TaskList = ({ loading, taskList, setTaskList }) => {
+  if (loading) {
+    return <h1>Loading in progress...</h1>
+  }
+  const onTaskCheckChange = (task) => {
+    // eslint-disable-next-line arrow-parens
+    setTaskList(taskList => taskList.map((singleTask) => {
+      if (singleTask._id === task._id) {
+        return { singleTask, isChecked: !singleTask.isChecked };
+      }
+      return singleTask;
+    }));
+  }
+  return (
+    <section>
+      {taskList.reverse().map((task) => (
+        <div key={task._id}>
+          <h4>{task.description}</h4>
+          <input onChange={() => onTaskCheckChange(task)} type="checkbox" checked={task.isChecked} />
+          <p>{formatRelative(task.date, new Date())}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default TaskList;
